07bab24b546dd4f8fc0c790b547c1dd29a9fe975
Dan Watson <dcwatson@gmail.com>
2020-05-20 13:29:22 -0400

    Move to beta

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=["cryptography", "tqdm"],
     entry_points={"console_scripts": ["pzip=pzip:main"]},
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
